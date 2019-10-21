553196c76ebbce6406509ade5d20bad14b1ba3c7
Ashkan <Ashkan@DESKTOP-HA7VPSF>
2019-10-21 18:24:09 +0300

    controller actions modified

--- NetCoreAssignment/NetCoreAssignment/Controllers/MoviesController.cs
+++ NetCoreAssignment/NetCoreAssignment/Controllers/MoviesController.cs
@@ -2,22 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Assignment.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NetCoreAssignment.Data;
 
 namespace NetCoreAssignment.Controllers
 {
+    [Authorize]
     public class MoviesController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         // GET: Movies
         public ActionResult Index()
         {
-            return View();
+            var movies = _context.Movies.ToList();
+            return View(movies);
         }
 
         // GET: Movies/Details/5
         public ActionResult Details(int id)
         {
+            var movie = _context.Movies.FirstOrDefault(x => x.Id == id);
             return View();
         }
 
@@ -30,11 +41,15 @@ namespace NetCoreAssignment.Controllers
         // POST: Movies/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create([FromForm] Movie movie)
         {
             try
             {
-                // TODO: Add insert logic here
+                if(ModelState.IsValid)
+                {
+                    _context.Movies.Add(movie);
+                    _context.SaveChanges();
+                }
 
                 return RedirectToAction(nameof(Index));
             }
@@ -47,23 +62,28 @@ namespace NetCoreAssignment.Controllers
         // GET: Movies/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var movie = _context.Movies.FirstOrDefault(x => x.Id == id);
+            return View(movie);
         }
 
         // POST: Movies/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, [FromForm] Movie movie)
         {
             try
             {
-                // TODO: Add update logic here
+                if (ModelState.IsValid)
+                {
+                    _context.Update(movie);
+                    _context.SaveChanges();
+                }
 
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                return View(movie);
             }
         }
 
@@ -76,17 +96,18 @@ namespace NetCoreAssignment.Controllers
         // POST: Movies/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, [FromForm] Movie movie)
         {
             try
             {
-                // TODO: Add delete logic here
+                _context.Remove(movie);
+                _context.SaveChanges();
 
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                return View(movie);
             }
         }
     }
