acd2356041215adfb282f29126d467590ebc6072
Ashkan <Ashkan@DESKTOP-HA7VPSF>
2019-10-21 17:40:56 +0300

    MovieModelView created

---
+++ NetCoreAssignment/NetCoreAssignment/Models/MoviesViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(50, ErrorMessage = "Movie Title can not exceed 50 characters.")]
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public double Rating { get; set; }
+        [MaxLength(1024, ErrorMessage = "Movie Title can not exceed 1024 characters.")]
+        public string Description { get; set; }
+        public int Year { get; set; }
+    }
+}
